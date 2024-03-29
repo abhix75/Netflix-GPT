6d286dee3e00090bb8ce1344a63693148eb49dbf
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-03-29 15:37:57 +0530

    Regiter TMDB API & create an app & get access token - Get Data from TMDB now playing movies list API

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,10 @@
 export const Logo = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 export const User_Avatar = "https://media.licdn.com/dms/image/D5603AQGZqXkruAI2gg/profile-displayphoto-shrink_400_400/0/1710064036228?e=1717027200&v=beta&t=sGyzytPv29Qrpwx3lNIopRnEAcArexfyyqO1Ovh2Gak";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3YjAwNGNmODA0Y2ViMzE3MmQwZGFmZTlhOTc0ZGRhMCIsInN1YiI6IjY2MDY4YzkwYTZkZGNiMDE3YzQ1MWVmNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VJy61uWeavXaR4Kc1Uapcoey1I-QRwr78CWbbfeoGdY'
+    }
+  };
