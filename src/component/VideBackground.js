5dd1efe066c89ca8b01b42ec94358b2cbd1e14e5
Abhijit Mishra <abhijitmishraak10@gmail.com>
2024-03-30 16:14:16 +0530

    clean the code and create a custom Hook UseMovieTrailer

--- src/component/VideBackground.js
+++ src/component/VideBackground.js
@@ -1,27 +1,9 @@
-import { useEffect } from "react";
-import { API_OPTIONS } from "../utils/constant";
-import { useDispatch, useSelector } from "react-redux";
-import { addTrailerVideos } from "../utils/movieSlice";
+import { useSelector } from "react-redux";
+import useMovieTrailer from "../hooks/useMovieTrailer";
 
 const VideoBackground = ({ movieId }) => {
   const trailerVideo = useSelector((store) => store?.movies?.trailerVideo);
-  const dispatch = useDispatch();
-  const getMovieTrailer = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/1011985/videos?language=en-US",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log("Trailer", json);
-
-    const filterData = json.results.filter((video) => video.type === "Trailer");
-    const trailer = filterData.length ? filterData[0] : json.results[0];
-    dispatch(addTrailerVideos(trailer));
-  };
-
-  useEffect(() => {
-    getMovieTrailer();
-  }, []);
+   useMovieTrailer(movieId);
   return (
     <div>
       <iframe
